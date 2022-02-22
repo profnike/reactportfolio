2b5542545b096c96d7dfd764addeef49a4d3a6dd
profnike <profnike@gmail.com>
2022-02-22 04:27:25 +0100

    further corrections and adjustment

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -28,8 +28,8 @@ function About (){
 
         </div>
         <div className="about-text">
-       <p className="div-parag"> I am a graduate of Nursing Science from the College of Medicine, University of Lagos. 
-        My interest in tech started when i attended a  Lagos state sponsored program. 
+       <p className="div-parag"> I am Onanuga Khadijah, a graduate of Nursing Science from the College of Medicine, University of Lagos. 
+        My interest in tech started when I attended a  Lagos state sponsored program. 
         The program, called "Code-Lagos", was for 2-weeks and it was  a program that exposed me 
         to HTML and CSS. On Completing the program I had a strong passion to go fully into programming and 
          get better at it.  
@@ -45,10 +45,10 @@ function About (){
            
             </div>
         <div className="remaining-parag" style={parag}>
-        <p className="div-parag">I believe in seeking help when needed, which made me enroll into programming schools when i realized 
+        <p className="div-parag">I believe in seeking help when needed, which made me enroll into programming schools when I realized 
             I was stuck while learning javascript on my own. That has been a great decision so far  as I didnt just  learn javascript,
-             I also learned new things in Html and Css as well  Reactjs(a javascript framework).   </p>
-             <p className="div-parag"> When i am not coding, I am surfing the internet trying to relax . </p>
+             I also learned new things in Html and Css as well  Reactjs(a javascript framework).    </p>
+             <p className="div-parag"> When I am not coding, I am surfing the internet trying to relax . </p>
              </div>
             < div className="remain-button" style={less} onClick={(()=>{setParag({display:"none"});setShow({display:"flex",backgroundColor:"teal",width:"150px"});setLess({display:"none"})})}>
                 <p>Show less</p>
